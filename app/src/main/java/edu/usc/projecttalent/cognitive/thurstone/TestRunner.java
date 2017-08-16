b4b5cf8c260f2ba1c5896e3e5cb327ae18f28cbd
Anindya Dutta <anindya.dutta22@gmail.com>
2017-08-16 02:39:15 -0700

    Refactor drawables and update TestRunner
    Refactor drawables and update TestRunner.

--- app/src/main/java/edu/usc/projecttalent/cognitive/thurstone/TestRunner.java
+++ app/src/main/java/edu/usc/projecttalent/cognitive/thurstone/TestRunner.java
@@ -1,6 +1,7 @@
 package edu.usc.projecttalent.cognitive.thurstone;
 
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -16,15 +17,7 @@ import edu.usc.projecttalent.cognitive.R;
 
 public class TestRunner extends BaseActivity {
 
-    int[] imageArray = {R.drawable.zero, R.drawable.jacket_one_dark, R.drawable.girl_four_dark, R.drawable.horse_one_dark, R.drawable.pumpkin_four_dark, R.drawable.umbrella_four_dark, R.drawable.chicken_two_dark,
-            R.drawable.duck_four_dark, R.drawable.elephant_one, R.drawable.girl_door_one, R.drawable.cake_two, R.drawable.fire_hat_one, R.drawable.baby_bird_one,
-            R.drawable.boat_three, R.drawable.ship_four, R.drawable.row_boat_two, R.drawable.clown_three, R.drawable.nest_four, R.drawable.party_hat_three,
-            R.drawable.girl_window_one, R.drawable.key_three, R.drawable.butterfly_three, R.drawable.chick_two, R.drawable.house_one, R.drawable.toys_three,
-            R.drawable.toy_egg_four, R.drawable.kennel_three, R.drawable.boots_two, R.drawable.box_four};
-
-    // Declare globally
     private int position = -1;
-    //This timer will call each of the seconds.
     Timer mTimer = new Timer();
 
     @Override
@@ -33,6 +26,7 @@ public class TestRunner extends BaseActivity {
         setContentView(R.layout.activity_thur_testrunner);
 
         final ImageView imageChangeReal = (ImageView) findViewById(R.id.imgeChangeReal);
+        TypedArray image = getResources().obtainTypedArray(R.array.th_questions);
 
         mTimer.schedule(new TimerTask() {
             @Override
@@ -40,14 +34,13 @@ public class TestRunner extends BaseActivity {
                 // As timer is not a Main/UI thread need to do all UI task on runOnUiThread
                 TestRunner.this.runOnUiThread(() -> {
                     position++;
-                    if (position >= imageArray.length) {
+                    if (position >= image.length()) {
                         startActivityForResult(new Intent(getApplicationContext(), TestAnswer.class), 1);
                         mTimer.cancel();
                     } else
-                        imageChangeReal.setImageResource(imageArray[position]);
+                        imageChangeReal.setImageResource(image.getResourceId(position, 0));
                 });
             }
         }, 0, 5000);
     }
 }
-
